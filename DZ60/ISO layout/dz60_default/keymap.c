19c24e5062e7995d6b2f16cc361e801f7427954b
Martin Ekberg <martin@mekberg.se>
2019-06-25 09:35:16 +0200

    Add box drawing chars to visualize keyboard

--- DZ60/ISO layout/dz60_default/keymap.c
+++ DZ60/ISO layout/dz60_default/keymap.c
@@ -1,7 +1,48 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_60_iso(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, LT(2,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_QUOT, KC_ENT, KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_MINS, KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_LGUI, KC_RALT, MO(1), KC_RCTL),
-	[1] = LAYOUT_60_iso(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO),
-	[2] = LAYOUT_60_iso(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+	[0] = LAYOUT_60_iso(
+/*  ┏━━━━━━━━━┳━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━━┳━━━━━┳━━━━━┳━━━━━━┓   */
+/*  ┃  Esc           ┃    1    ┃     2     ┃    3    ┃     4     ┃     5     ┃    6    ┃     7     ┃    8    ┃    9      ┃    0      ┃    +    ┃    ´    ┃ Backspace ┃   */
+		KC_GESC,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,	KC_EQL,		KC_BSPC,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━╋━━━━━━┫   */
+/*  ┃  Tab/Layer 2   ┃    Q    ┃     W     ┃    E    ┃     R     ┃    T      ┃    Y    ┃     U     ┃    I    ┃    O      ┃    P      ┃   Å     ┃    ¨    ┃   Enter   ┃   */
+		LT(2,KC_TAB), 	KC_Q, 		KC_W, 		KC_E, 		KC_R, 		KC_T, 		KC_Y, 		KC_U, 		KC_I, 		KC_O, 		KC_P, 		KC_LBRC, 	KC_RBRC,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━┫           ┃   */
+/*  ┃  LCtrl         ┃    A    ┃     S     ┃    D    ┃     F     ┃    G      ┃    H    ┃     J     ┃    K    ┃    L      ┃    Ö      ┃   Ä     ┃    '    ┃           ┃   */
+		KC_LCTL,		KC_A, 		KC_S, 		KC_D, 		KC_F, 		KC_G, 		KC_H, 		KC_J, 		KC_K, 		KC_L, 		KC_SCLN, 	KC_QUOT, 	KC_QUOT,	KC_ENT,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━┻━━━━━━┫   */
+/*  ┃  LShift        ┃    <>   ┃     Z     ┃    X    ┃     C     ┃     V     ┃    B    ┃     N     ┃    M    ┃     ,     ┃     .     ┃    -    ┃        RShift        ┃   */
+		KC_LSFT, 		KC_NUBS,	KC_Z, 		KC_X, 		KC_C, 		KC_V, 		KC_B, 		KC_N, 		KC_M, 		KC_COMM,	KC_DOT,		KC_MINS, 		KC_RSFT,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━┻━━━━━━┻━━━━━━┻━━━━━┻━━━━━━┻━━━━━┻━━━━━━╋━━━━━━╋━━━━━╋━━━━━┳━━━━━━┫   */
+/*  ┃  LCtrl         ┃   LAlt  ┃  Command  ┃                                     Space                                        ┃  Command  ┃  RAlt   ┃    Fn   ┃   RCtrl   ┃   */
+		KC_LCTL, 		KC_LALT, 	KC_LGUI, 										KC_SPC, 										KC_LGUI, 	KC_RALT, 	MO(1), 		KC_RCTL),
+/*	┗━━━━━━━━━┻━━━━━┻━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━┻━━━━━┻━━━━━┻━━━━━━┛   */
+
+	[1] = LAYOUT_60_iso(
+/*  ┏━━━━━━━━━┳━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━━┳━━━━━┳━━━━━┳━━━━━━┓   */
+		KC_ESC, 		KC_F1, 		KC_F2, 		KC_F3, 		KC_F4, 		KC_F5, 		KC_F6, 		KC_F7, 		KC_F8, 		KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_DEL,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━╋━━━━━━┫   */
+		KC_NO, 			KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━┫           ┃   */
+		KC_CAPS, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━┻━━━━━━┫   */
+		KC_NO,			KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 			KC_NO,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━┻━━━━━━┻━━━━━━┻━━━━━┻━━━━━━┻━━━━━┻━━━━━━╋━━━━━━╋━━━━━╋━━━━━┳━━━━━━┫   */
+		KC_NO, 			KC_NO, 		KC_NO, 											KC_NO, 											KC_NO, 		KC_NO, 		KC_TRNS, 	KC_NO),
+/*	┗━━━━━━━━━┻━━━━━┻━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━┻━━━━━┻━━━━━┻━━━━━━┛   */
+
+	[2] = LAYOUT_60_iso(
+/*  ┏━━━━━━━━━┳━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━┳━━━━━━┳━━━━━━┳━━━━━┳━━━━━┳━━━━━━┓   */
+/*  ┃  Esc           ┃         ┃           ┃         ┃           ┃           ┃         ┃           ┃         ┃           ┃           ┃         ┃         ┃           ┃   */
+		KC_NO, 			KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_DEL,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━╋━━━━━━┫   */
+		KC_TRNS, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,		KC_NO, 		KC_HOME, 	KC_UP, 		KC_END, 	KC_PGUP, 	KC_NO, 		KC_NO,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━┫           ┃   */
+		KC_NO, 			KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_LEFT, 	KC_DOWN, 	KC_RGHT, 	KC_PGDN, 	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━╋━━━━━━╋━━━━━━╋━━━━━╋━━━━━┻━━━━━━┫   */
+		KC_NO, 			KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
+/*	┣━━━━━━━━━╋━━━━━╋━━━━━━╋━━━━━┻━━━━━━┻━━━━━━┻━━━━━┻━━━━━━┻━━━━━┻━━━━━━╋━━━━━━╋━━━━━╋━━━━━┳━━━━━━┫   */
+		KC_NO, 			KC_NO, 		KC_NO, 											KC_NO, 											KC_NO, 		KC_NO,		KC_NO, 		KC_NO)
+/*	┗━━━━━━━━━┻━━━━━┻━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━┻━━━━━┻━━━━━┻━━━━━━┛   */
 };
